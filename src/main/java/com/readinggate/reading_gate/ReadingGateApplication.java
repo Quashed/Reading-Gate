a149e073bc227be0cdc503f225770129fffa5d34
Hisam Iftikhar <hisamiftikhar@gmail.com>
2026-01-15 17:35:46 +0000

    Initial Spring Boot project scaffold

---
+++ src/main/java/com/readinggate/reading_gate/ReadingGateApplication.java
@@ -0,0 +1,13 @@
+package com.readinggate.reading_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readinggate/reading_gate/ReadingGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readinggate.reading_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
